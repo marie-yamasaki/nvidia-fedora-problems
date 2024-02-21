252828d17b97567d466a29166ebed4a3e9a06d4f
marie-yamasaki <150583204+marie-yamasaki@users.noreply.github.com>
2024-02-21 12:13:17 +0100

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -2,5 +2,6 @@
 sudo dnf remove *nvidia* --noautoremove --exclude=nvidia-gpu-firmware
 sudo dnf install akmod-nvidia-470xx --disablerepo rpmfusion-nonfree-nvidia-driver --enablerepo rpmfusion-nonfree
 # sleep 5min 
+echo -e "\n Sleeping for 5 min \n"
 sleep 300 #seconds / 5min
 dnf list installed *nvidia*
